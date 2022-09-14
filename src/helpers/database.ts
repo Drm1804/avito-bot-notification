a7872967f594be0700a8be369ef810d3b8e30031
Di. Per <aleksey.ustinov@corp.mail.ru>
2022-09-14 16:19:53 +0300

    video 002

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -1,11 +1,12 @@
 import { FirebaseApp, initializeApp } from 'firebase/app';
-import { Database, getDatabase, ref, set } from 'firebase/database';
+import { Database, getDatabase, ref, set, get, child, onChildAdded } from 'firebase/database';
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
 import { conf } from '../../config.js';
 
 class DatabaseService {
   app: FirebaseApp
   db: Database
+  initSkip = true;
 
   constructor() {
     try{
@@ -27,11 +28,42 @@ class DatabaseService {
     }
   }
 
-  writeMessage(mes: string, time: number): Promise<void> {
+
+  // получение всех юзеров для оповещения
+  getUsers(): Promise<Collection<User>> {
     return new Promise((resolve, reject) => {
-      set(ref(this.db, 'messages/' + String(time)), {
-        mes
-      }).then(resolve, reject).catch(reject)
+      get(child(ref(this.db), 'users'))
+        .then((snapshot) => resolve(snapshot.val()))
+        .catch(err => reject(err))
+    })
+  }
+
+
+  // добавление слушателя оповещений
+  setUserListner(user: User): Promise<void> {
+    return new Promise((resolve, reject) => {
+      set(ref(this.db, 'users/' + user.id), user)
+      .then(() => resolve())
+      .catch(err => reject(err))
+    })
+  }
+  // подписка на обновления объявлений
+
+  async updateAds(cb): Promise<void> {
+    onChildAdded(ref(this.db, 'ads'), (snapshot) => {
+      const data: Collection<Ad> = snapshot.val();
+
+      // При первом запуске начинают лететь уже добавленные в базу поля
+      // мы их скипаем, чтобы следить только за новыми
+      setTimeout(() => {
+        this.initSkip = false;
+      })
+
+      if(this.initSkip) {
+        return
+      }
+
+      cb(data);
     })
   }
 }
@@ -39,3 +71,20 @@ class DatabaseService {
 const db = new DatabaseService();
 export default db;
 
+export interface Collection<T> {
+  [key: string]: T
+}
+
+export interface User {
+  id: number,
+  is_bot: boolean,
+  first_name: string,
+  username: string
+}
+
+export interface Ad {
+  title: string,
+  id: string,
+  price: number,
+  url: string
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { conf } from '../config.js';
 import { Scenes, Telegraf } from 'telegraf';
-import db from './helpers/database.js'
+import db, { Ad, User } from './helpers/database.js'
 import { pause } from './helpers/utils.js'
 import logger from './helpers/logger.js'
 import { Logger } from 'log4js';
@@ -11,11 +11,35 @@ const bot = new Telegraf<Scenes.SceneContext>(conf.botToken);
 (async (): Promise<void> => {
   const _logger: Logger = logger.get('Main')
   await pause(1000);
+
+  let users = await db.getUsers();
+  let usersIds = users ? Object.keys(users) : [];
+
   bot.on('text', async (ctx) => {
-    _logger.info('trigger бота')
-    await db.writeMessage(ctx.update.message.text, ctx.update.message.date)
-    ctx.reply('Привет')
+    const { from } = ctx.update.message;
+    _logger.info('Мне написал пользователь ' + from.id)
+    await db.setUserListner(from as User);
+    ctx.reply('Я добавил тебя в рассылку');
+    _logger.info(`Добавил пользователь ${from.id} ${from.username} в рассылку`)
+    users = await db.getUsers();
+    usersIds = users ? Object.keys(users) : [];
+    _logger.info('Обновил список юзеров для рассылки')
   })
+
+
+  function notifyUser(data: Ad): void {
+    _logger.info('Произошло обновление списка объявлений');
+    const text = `Появился новый товар ${data.title} c ценой ${data.price} руб
+Ссылка на объявление https://avito.ru${data.url}`;
+
+    for(const id of usersIds) {
+      bot.telegram.sendMessage(id, text);
+
+      _logger.info('Выслал оповещение пользователю' + id);
+    }
+  }
+
+  db.updateAds(notifyUser)
 
   bot.launch();
 })()
